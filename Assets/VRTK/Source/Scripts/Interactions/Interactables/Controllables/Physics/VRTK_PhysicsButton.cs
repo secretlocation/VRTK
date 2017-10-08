debd1521a374e7d7ccc2c80c313ac2cb5da63e13
Harvey Ball <me@harveyball.com>
2017-10-08 11:22:12 +0100

    feat(Interaction): add new button controllable replacing 3d control
    The 3D Control button has now been deprecated in the first commit of
    deprecating all of the existing 3D controls as they are being replaced
    with the new Controllables controls. The new Controllable Button works
    in a similar fashion to the original 3D Button but is more modular
    and can be enabled/disabled at runtime and cleans up correctly.
    
    The new Controllables are to be separated into two concepts of Physics
    based and Artificial based, with Physics controls using the Unity
    Physics system to move and the Artificial controls not using physics
    and just simulating the concept of a control.

---
+++ Assets/VRTK/Source/Scripts/Interactions/Interactables/Controllables/Physics/VRTK_PhysicsButton.cs
@@ -0,0 +1,219 @@
+﻿// Physics Button|PhysicsControllables|110020
+namespace VRTK.Controllables.PhysicsBased
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// A physics based pushable button.
+    /// </summary>
+    /// <remarks>
+    /// **Required Components:**
+    ///  * `Collider` - A Unity Collider to determine when an interaction has occured. Can be a compound collider set in child GameObjects. Will be automatically added at runtime.
+    ///  * `Rigidbody` - A Unity Rigidbody to allow the GameObject to be affected by the Unity Physics System. Will be automatically added at runtime.
+    ///
+    /// **Optional Components:**
+    ///  * `VRTK_ControllerRigidbodyActivator` - A Controller Rigidbody Activator to automatically enable the controller rigidbody upon touching the button. Will be automatically created if the `Auto Interaction` paramter is checked.
+    /// 
+    /// **Script Usage:**
+    ///  * Place the `VRTK_PhysicsButton` script onto the GameObject that is to become the button.
+    /// </remarks>
+    [AddComponentMenu("VRTK/Scripts/Interactables/Controllables/Physics/VRTK_PhysicsButton")]
+    public class VRTK_PhysicsButton : VRTK_BasePhysicsControllable
+    {
+        [Header("Button Settings")]
+
+        [Tooltip("The distance along the `Operate Axis` until the button reaches the pressed position.")]
+        public float pressedDistance = 0.1f;
+        [Range(0f, 1f)]
+        [Tooltip("The threshold in which the button's current normalized position along the `Operate Axis` has to be within the pressed normalized position for the button to be considered pressed.")]
+        public float pressedThreshold = 0f;
+        [Tooltip("If this is checked then the button will stay in the pressed position when it reaches the pressed position.")]
+        public bool stayPressed = false;
+        [Tooltip("The position of the button between the original position and the pressed position. `0f` will set the button position to the original position, `1f` will set the button position to the pressed position.")]
+        [Range(0f, 1f)]
+        public float positionTarget = 0f;
+        [Tooltip("The amount of force to apply to push the Button towards the `Target Position`")]
+        public float targetForce = 10f;
+
+        protected ConfigurableJoint controlJoint;
+        protected bool createCustomJoint;
+
+        protected Vector3 targetLocalPosition;
+        protected Vector3 previousLocalPosition;
+        protected bool pressedDown;
+
+        /// <summary>
+        /// The GetValue method returns the current position value of the button.
+        /// </summary>
+        /// <returns>The actual position of the button.</returns>
+        public override float GetValue()
+        {
+            return transform.localPosition[(int)operateAxis];
+        }
+
+        /// <summary>
+        /// The GetNormalizedValue method returns the current position value of the button normalized between `0f` and `1f`.
+        /// </summary>
+        /// <returns>The normalized position of the button.</returns>
+        public override float GetNormalizedValue()
+        {
+            return VRTK_SharedMethods.NormalizeValue(GetValue(), originalLocalPosition[(int)operateAxis], PressedPosition()[(int)operateAxis]);
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            SetupJoint();
+            targetLocalPosition = Vector3.zero;
+            previousLocalPosition = transform.localPosition;
+            pressedDown = false;
+        }
+
+        protected override void OnDisable()
+        {
+            if (createCustomJoint)
+            {
+                Destroy(controlJoint);
+            }
+            base.OnDisable();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            SetRigidbodyVelocity(Vector3.zero);
+            ForceLocalPosition();
+        }
+
+        protected virtual void Update()
+        {
+            if (transform.localPosition != previousLocalPosition)
+            {
+                OnValueChanged(EventPayload());
+                CheckPressEvents();
+                previousLocalPosition = transform.localPosition;
+            }
+
+            if (!stayPressed && pressedDown)
+            {
+                SetRigidbodyConstraints(RigidbodyConstraints.FreezeRotation);
+            }
+
+            SetTargetPosition();
+        }
+
+        protected override void OnDrawGizmosSelected()
+        {
+            base.OnDrawGizmosSelected();
+            Vector3 transformPosition = (Application.isPlaying ? originalPosition : transform.position);
+            Vector3 objectHalf = AxisDirection(true) * (transform.lossyScale[(int)operateAxis] * 0.5f);
+            Vector3 initialPoint = transformPosition + (objectHalf * Mathf.Sign(pressedDistance));
+            Vector3 destinationPoint = initialPoint + (AxisDirection(true) * pressedDistance);
+            Gizmos.DrawLine(initialPoint, destinationPoint);
+            Gizmos.DrawSphere(destinationPoint, 0.01f);
+        }
+
+        protected override void ConfigueRigidbody()
+        {
+            SetRigidbodyGravity(false);
+            SetRigidbodyCollisionDetectionMode(CollisionDetectionMode.ContinuousDynamic);
+            SetRigidbodyConstraints(RigidbodyConstraints.FreezeRotation);
+        }
+
+        protected virtual void ForceLocalPosition()
+        {
+            float xPos = (operateAxis == OperatingAxis.xAxis ? transform.localPosition.x : originalLocalPosition.x);
+            float yPos = (operateAxis == OperatingAxis.yAxis ? transform.localPosition.y : originalLocalPosition.y);
+            float zPos = (operateAxis == OperatingAxis.zAxis ? transform.localPosition.z : originalLocalPosition.z);
+            transform.localPosition = new Vector3(xPos, yPos, zPos);
+        }
+
+        protected virtual void SetTargetPosition()
+        {
+            if (controlJoint != null)
+            {
+                controlJoint.targetPosition = (AxisDirection() * Mathf.Sign(pressedDistance)) * Mathf.Lerp(controlJoint.linearLimit.limit, -controlJoint.linearLimit.limit, positionTarget);
+            }
+        }
+
+        protected virtual Vector3 PressedPosition()
+        {
+            return originalLocalPosition + (AxisDirection() * pressedDistance);
+        }
+
+        protected virtual void SetupJoint()
+        {
+            //move transform towards activation distance
+            transform.localPosition += AxisDirection() * (pressedDistance * 0.5f);
+
+            controlJoint = GetComponent<ConfigurableJoint>();
+            createCustomJoint = false;
+            if (controlJoint == null)
+            {
+                controlJoint = gameObject.AddComponent<ConfigurableJoint>();
+                createCustomJoint = true;
+
+                controlJoint.angularXMotion = ConfigurableJointMotion.Locked;
+                controlJoint.angularYMotion = ConfigurableJointMotion.Locked;
+                controlJoint.angularZMotion = ConfigurableJointMotion.Locked;
+
+                controlJoint.xMotion = (operateAxis == OperatingAxis.xAxis ? ConfigurableJointMotion.Limited : ConfigurableJointMotion.Locked);
+                controlJoint.yMotion = (operateAxis == OperatingAxis.yAxis ? ConfigurableJointMotion.Limited : ConfigurableJointMotion.Locked);
+                controlJoint.zMotion = (operateAxis == OperatingAxis.zAxis ? ConfigurableJointMotion.Limited : ConfigurableJointMotion.Locked);
+
+                JointDrive snapDriver = new JointDrive();
+                snapDriver.positionSpring = 1000f;
+                snapDriver.positionDamper = 10f;
+                snapDriver.maximumForce = targetForce;
+
+                controlJoint.xDrive = snapDriver;
+                controlJoint.yDrive = snapDriver;
+                controlJoint.zDrive = snapDriver;
+
+                SoftJointLimit linearLimit = new SoftJointLimit();
+                linearLimit.limit = Mathf.Abs(pressedDistance * 0.5f);
+                controlJoint.linearLimit = linearLimit;
+                controlJoint.connectedBody = connectedTo;
+            }
+        }
+
+        protected virtual void CheckPressEvents()
+        {
+            float currentPosition = GetNormalizedValue();
+            ControllableEventArgs payload = EventPayload();
+            if (currentPosition >= (1f - pressedThreshold) && !AtMaxLimit())
+            {
+                atMaxLimit = true;
+                OnMaxLimitReached(payload);
+                StickOnPressed();
+            }
+            else if (currentPosition <= (0f + pressedThreshold) && !AtMinLimit())
+            {
+                atMinLimit = true;
+                OnMinLimitReached(payload);
+            }
+            else if (currentPosition > pressedThreshold && currentPosition < (1f - pressedThreshold))
+            {
+                if (AtMinLimit())
+                {
+                    OnMinLimitExited(payload);
+                }
+                if (AtMaxLimit())
+                {
+                    OnMaxLimitExited(payload);
+                }
+
+                atMinLimit = false;
+                atMaxLimit = false;
+            }
+        }
+
+        protected virtual void StickOnPressed()
+        {
+            if (stayPressed)
+            {
+                SetRigidbodyConstraints(RigidbodyConstraints.FreezeAll);
+                pressedDown = true;
+            }
+        }
+    }
+}
